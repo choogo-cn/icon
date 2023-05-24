52b75a2f3c0d289509dd1792f68f97f4af06933e
JetLu <i@lufei.im>
2023-05-24 15:13:01 +0800

    feat: 支持更多配置

--- src/lib/Root.svelte
+++ src/lib/Root.svelte
@@ -3,6 +3,20 @@
   export let color = '#ccc'
   let cls: HTMLAttributes<SVGAElement>['class'] = undefined
   export {cls as class}
+  export let onClick: (() => void) | undefined = undefined
+  export let width = 24
+  export let height = width
 </script>
 
-<svg stroke={color} class={cls} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><slot/></svg>
+<svg
+  on:click={onClick}
+  stroke={color}
+  class={cls}
+  width={width}
+  height={height}
+  viewBox="0 0 24 24"
+  stroke-width="2"
+  fill="none"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+><slot/></svg>
