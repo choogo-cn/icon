7bb07ef1ea747fb49543d222cce3845aa893a752
JetLu <i@lufei.im>
2023-05-24 14:52:40 +0800

    rm

--- .svelte-kit/generated/client/nodes/0.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/+layout.ts";
-export { universal };
-export { default as component } from "../../../../../../node_modules/.pnpm/@sveltejs+kit@1.18.0_svelte@3.59.1_vite@4.3.8/node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.71b508d9.js","_app/immutable/chunks/index.a14f1cf4.js","_app/immutable/chunks/singletons.d7bd2132.js"];
-export const stylesheets = [];
-export const fonts = [];
