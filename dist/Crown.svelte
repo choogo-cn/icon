0cb10eb946f9b3ace8f3415efb3d7a940c112a31
JetLu <i@lufei.im>
2023-05-24 14:54:16 +0800

    rm

--- dist/Crown.svelte
+++
@@ -1,6 +0,0 @@
-<script>import Root from "./Root.svelte";
-</script>
-
-<Root {...$$props}>
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z"/>
-</Root>
--- dist/Root.svelte
+++
@@ -1,6 +0,0 @@
-<script>export let color = "#ccc";
-let cls = void 0;
-export { cls as class };
-</script>
-
-<svg stroke={color} class={cls} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><slot/></svg>
