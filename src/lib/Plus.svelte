101e99ed27e77ca7a501c69f415860bb49e0fc15
JetLu <i@lufei.im>
2023-05-25 10:12:22 +0800

    feat: add plus

---
+++ src/lib/Plus.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type {ComponentProps} from 'svelte'
+  import Root from './Root.svelte'
+  interface $$Props extends ComponentProps<Root> {}
+</script>
+
+<Root {...$$props}>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M12 5l0 14"></path>
+  <path d="M5 12l14 0"></path>
+</Root>
