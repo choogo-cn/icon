1981aabefbda021c3f22a90e8743994c187d4e11
JetLu <i@lufei.im>
2023-05-25 14:54:27 +0800

    add: close

---
+++ src/lib/Close.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type {ComponentProps} from 'svelte'
+  import Root from './Root.svelte'
+  interface $$Props extends ComponentProps<Root> {}
+</script>
+
+<Root {...$$props}>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M18 6l-12 12"></path>
+  <path d="M6 6l12 12"></path>
+</Root>
