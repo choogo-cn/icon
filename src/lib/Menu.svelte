fa495a5d0778c8ad72936936f228ebe9e3e07b2d
JetLu <i@lufei.im>
2023-11-17 01:01:51 +0800

    feat: menu

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type {ComponentProps} from 'svelte'
+  import Root from './Root.svelte'
+  interface $$Props extends ComponentProps<Root> {}
+  export let target: HTMLElement | SVGElement | null = null
+</script>
+
+<Root {...$$props} bind:target={target}>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0"/><path d="M4 12l16 0"/><path d="M4 18l16 0"/>
+</Root>
